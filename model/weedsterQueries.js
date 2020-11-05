85806c552a2484937451a8e59d72339d96d10397
SJS <sjsevadjian@gmail.com>
2020-11-04 21:08:42 -0800

    added more queries

--- model/weedsterQueries.js
+++ model/weedsterQueries.js
@@ -1,23 +1,30 @@
-const queryPostsByCategory = 'SELECT title, message FROM posts WHERE category = ?;';
-const queryMyPosts = 'SELECT title, message FROM post INNER JOIN users ON post.userId = users.id;';
+// Get all
+const getAllPosts = 'SELECT title, caption FROM post;';
+
+// Get one
+const getSinglePost = 'SELECT title, caption FROM post WHERE id = ?;';
+const getAllPostsByCategory = 'SELECT title, caption FROM post WHERE category = ?;';
+
+// Get all post of specific user (does not show comments, query post to see post & comments)
+const getAllUserPosts = 'SELECT title, caption FROM post INNER JOIN users ON post.userId = ? AND post.userId = users.id;';
+
+// Getting all of the posts and the comments that belong to them
+//Does not retrieve post if it has no comments
+const getAllPostsAndComments = 'SELECT post.title, post.caption, comments.message FROM post INNER JOIN comments ON post.id = comments.postId;';
+
+// Getting one post and all of the comments that belong to it
+const getSinglePostandComments = 'SELECT post.title, post.caption, comments.message FROM post INNER JOIN comments ON post.id = ? AND post.id = comments.postId;';
+
+// Post for creating (user will need to sign in to create)
+const insertPost = 'INSERT INTO post (title, category, caption, userId) values ( ?, ?, ?, ? );';
+
+// Creating a comment for a post by a specific user
+const insertComment = 'INSERT INTO comments (message, postId, userId) VALUES (?, ?, ?);';
+
+// Del for deleting
 const deletePost = 'DELETE FROM post WHERE id = ?;';
 const deleteComment = 'DELETE FROM comments WHERE id = ?;';
-const queryPostAndComments = 'SELECT message FROM comments INNER JOIN post ON comments.postId = post.id;';
 
 
-// Get all
-// Get one
-// Post for creating
-// Del for deleting
-// Getting all of the posts and the comments that belong to them ** << line 5
-// Getting one post and all of the comments that belong to it
-// Getting all the post of a specific user
-// Creating a post by a specific user
-// Creating a comment for a post by a specific user
-
-// For the ORM these are the data that should be returned for each ORM
-// Getting all of the posts and the comments that belong to them :   Should return an array
-// Getting one post and all of the comments that belong to it: Should return  an object
-// Getting all the post of a specific user : Should return the all   post of that user
-// Creating a post by a specific user: Should return the post that was just created
-// Creating a comment for a post by a specific user: Should return the comment that was just created
+// Creating a post by a specific user ** line 19
+// Creating a comment for a post by a specific user ** line 22
