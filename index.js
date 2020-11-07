65da812bf5486bca2d33d3b351d459585f4e0ff6
aprzygocki <augustino.przygocki@yahoo.com>
2020-11-07 11:27:17 -0800

    modified index.js and model

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log('Server started listening on PORT http://localhost:3001');
+  console.log('Server started listening on PORT', PORT);
 });
 
 
