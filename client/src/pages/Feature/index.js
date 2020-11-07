138b42aa1aac365300d7701e08e72564bf4020c2
aprzygocki <augustino.przygocki@yahoo.com>
2020-11-07 13:42:03 -0800

    redoing pull

--- client/src/pages/Feature/index.js
+++ client/src/pages/Feature/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import {useSelector} from 'react-redux';
 
 export default function SimpleContainer() {
+  const { token } = useSelector(state => state.viewer);
+  console.log("token",token)
   return (
     <React.Fragment>
       <CssBaseline />
