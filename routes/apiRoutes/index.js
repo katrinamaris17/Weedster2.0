24efeb1182f41e9adb1941ab591a2d69831418a8
aprzygocki <augustino.przygocki@yahoo.com>
2020-11-09 20:14:03 -0800

    Created feed controller and new routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { postApi } = require('../../controllers/postController');
 
-router.post('/post', postApi);
+const { feedApi } = require('../../controllers/feedController');
 
+router.post('/post', postApi);
+router.get('/get', feedApi);
 // Setup your routes for /api/something here
 // This line of code makes it so that /api/fweets is prepended to fweetRoutes
 // example route.use('/myRoute', myRoutes);
