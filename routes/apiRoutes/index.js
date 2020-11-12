89763e517dcf3981f372a26d82c23672ccb9e021
aprzygocki <augustino.przygocki@yahoo.com>
2020-11-11 18:53:41 -0800

    added to the post controller

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,9 +5,9 @@ const { feedApi } = require('../../controllers/feedController');
 
 const { categoryApi } = require ('../../controllers/categoryController');
 
-router.post('/post', categoryApi);
+router.post('/category', categoryApi);
 router.post('/post', postApi);
-router.get('/post', feedApi);
+router.get('/feed', feedApi);
 // Setup your routes for /api/something here
 // This line of code makes it so that /api/fweets is prepended to fweetRoutes
 // example route.use('/myRoute', myRoutes);
