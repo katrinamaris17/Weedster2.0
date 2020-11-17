7bd2d88295d39556058ef38b88c13558102a1bab
SJS <sjsevadjian@gmail.com>
2020-11-16 17:20:24 -0800

    testing

--- client/src/pages/common/components/Likes.js
+++ client/src/pages/common/components/Likes.js
@@ -1,19 +1,30 @@
 import React, {Component} from 'react';
 
-class Likes extends Component {
-    state = {
-        likes: 0
+class Likes extends React.Component {
+
+    render() {
+      return <h1>{this.props.likes}</h1>;
     }
-  handleLike(event, value) {
-    this.setState({likes: value + 1});
   }
-  render() {
-    return (
-        <div>
-                    <button onClick={this.handleLike}><span class="fas fa-thumbs-up"></span></button>
-                    {this.state.likes}
-        </div>
-    );
-  }
-}
-export default Likes;
+  
+  Likes.defaultProps = {
+    likes: 0,
+  };
+
+// class Likes extends Component {
+//     state = {
+//         likes: 0
+//     }
+//   handleLike(event, value) {
+//     this.setState({likes: value + 1});
+//   }
+//   render() {
+//     return (
+//         <div>
+//                     <button onClick={this.handleLike}><span class="fas fa-thumbs-up"></span></button>
+//                     {this.state.likes}
+//         </div>
+//     );
+//   }
+// }
+// export default Likes;
