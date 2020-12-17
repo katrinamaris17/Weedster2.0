ddcdd2303e76e5ff9961925c60eb0b1f742ccbe5
Katrina Arando <katrinamaris17@gmail.com>
2020-12-16 22:07:11 -0800

    deleted files

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,15 +2,15 @@ const router = require('express').Router();
 const authMiddleware = require('./../../middlewares/authorizationMiddleware')
 const { createPost, getAllPosts, createComment, removePost, hideComment } = require('../../controllers/postController');
 
-const { feedApi } = require('../../controllers/feedController');
+// const { feedApi } = require('../../controllers/feedController');
 
-const { categoryApi } = require ('../../controllers/categoryController');
+// const { categoryApi } = require ('../../controllers/categoryController');
 const { uploadImage } = require ('../../controllers/imageController');
 
-router.post('/category', categoryApi);
+// router.post('/category', categoryApi);
 router.post('/post', authMiddleware, createPost);
 router.get('/post', authMiddleware, getAllPosts);
-router.get('/feed', feedApi);
+// router.get('/feed', feedApi);
 router.delete('/post/:post_id', removePost)
 router.post('/comment', authMiddleware, createComment);
 router.put('/comment/:post_id/:comment_id', hideComment);
